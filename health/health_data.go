d8f621a229e8db3983655c2840e4657c1cb7d812
Ananda Fitra <anandadf@student.ub.ac.id>
2023-11-14 20:53:09 +0700

    feat: health data

---
+++ health/health_data.go
@@ -0,0 +1,92 @@
+package health
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/FinalProjectPSI-F-Kelompok2/dugdugsehat-backend/model"
+	"github.com/gin-gonic/gin"
+)
+
+type SaveDataRequest struct {
+	Email string `json:"email"`
+	Data  struct {
+		Ecg float32 `json:"ecg"`
+		Hr  int     `json:"hr"`
+	} `json:"data"`
+}
+
+type HealthData struct {
+	Date time.Time `sql:"measure_date"`
+	Ecg  float32   `sql:"ecg"`
+	Hr   int       `sql:"heart_rate"`
+}
+
+func SaveHealthData(db *model.DbCon) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var sdr SaveDataRequest
+		if c.BindJSON(&sdr) != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"status": "error in server",
+			})
+			return
+		}
+		dataTimestamp := time.Now()
+		r, err := db.Db.Query("INSERT INTO health_data VALUES ($1, $2, $3, $4)", sdr.Email, dataTimestamp, sdr.Data.Ecg, sdr.Data.Hr)
+		r.Close()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"status": "db error",
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	}
+}
+
+func GetHealthData(db *model.DbCon) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		email := c.GetHeader("email")
+		rowLimit := c.GetHeader("row")
+
+		// check if email is registered
+		var err error
+
+		var emailChk string
+		db.Db.QueryRow("SELECT email FROM profile WHERE email=$1", email).Scan(&emailChk)
+		if len(emailChk) <= 0 {
+			c.JSON(http.StatusNotFound, gin.H{
+				"status": "user not found",
+			})
+			return
+		}
+
+		r, err := db.Db.Query("SELECT measure_date, ecg, heart_rate FROM health_data WHERE email=$1 ORDER BY measure_date DESC LIMIT $2", email, rowLimit)
+		defer r.Close()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"status": err.Error(),
+			})
+			return
+		}
+
+		var res []HealthData
+
+		fmt.Println(res)
+
+		for r.Next() {
+			var hd HealthData
+			r.Scan(&hd.Date, &hd.Ecg, &hd.Hr)
+			res = append(res, hd)
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"email": email,
+			"row":   len(res),
+			"data":  res,
+		})
+	}
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/FinalProjectPSI-F-Kelompok2/dugdugsehat-backend/health"
 	"github.com/FinalProjectPSI-F-Kelompok2/dugdugsehat-backend/model"
 	"github.com/FinalProjectPSI-F-Kelompok2/dugdugsehat-backend/profile"
 	"github.com/gin-gonic/gin"
@@ -20,8 +21,14 @@ func main() {
 	// User Login
 	router.POST("/login", profile.Login(&db))
 	router.POST("/register", profile.Register(&db))
-	router.POST("/profile", profile.GetProfile(&db))
-	router.POST("/edit-profile", profile.UpdateProfile(&db))
+
+	// User Profile
+	router.GET("/profile", profile.GetProfile(&db))
+	router.POST("/profile", profile.UpdateProfile(&db))
+
+	// Health Data
+	router.GET("/health-data", health.GetHealthData(&db))
+	router.POST("/health-data", health.SaveHealthData(&db))
 	router.Run(":8080")
 
 	db.CloseDb()
